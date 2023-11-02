0d10ddc6b07151f0975780fd849a8b3281ae5d1c
Kamanda <erickckamanda@gmail.com>
2023-11-02 07:39:14 +0000

    Create a binary tree node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - create a binary tree node
+ * @parent: the address of the parent node of a subtree
+ * @value: the value of a node
+ * Return: return the address of the created node
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	new_node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+
+	return (new_node);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -15,10 +15,10 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s binary_tree_t;
@@ -26,4 +26,5 @@ typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 #endif
