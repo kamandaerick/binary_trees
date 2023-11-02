d9605847402b32dbee79d9b6566db05102866832
Kamanda <erickckamanda@gmail.com>
2023-11-02 17:25:51 +0000

    Find the depth and size of a binary tree

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_depth - measure the depth of a node
+ * @tree: pointer to the node to measure
+ * Return: return 0 if tree is NULL, otherwie return depth
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	if (tree && tree->parent)
+		return 1 + binary_tree_depth(tree->parent);
+	else
+		return (0);
+}
---
+++ 11-binary_tree_size.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_size - measure the size of a binary tree
+ * @tree: the tree whose size to measure
+ * Return: return the size of the tree or NULL if tree is NULL
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t left_nodes = 0;
+	size_t right_nodes = 0;
+
+	if (tree == NULL)
+		return (0);
+	else
+	{
+		left_nodes += binary_tree_size(tree->left);
+		left_nodes += binary_tree_size(tree->right);
+	}
+	return (1 + left_nodes + right_nodes);
+}
