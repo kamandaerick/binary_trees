e2fdde24f3c7ca1344535834692967f48a567e7f
Kamanda <erickckamanda@gmail.com>
2024-06-05 05:40:28 -0700

    Fix betty documentation errors

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -7,28 +7,29 @@
  *
  * Return: A pointer to the created node, or NULL on failure
  */
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value){
-    binary_tree_t *newNode;
-    binary_tree_t *currentNode;
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newNode;
+	binary_tree_t *currentNode;
 
-    if (parent == NULL) return (NULL);
-
-    newNode = malloc(sizeof(binary_tree_t));
-    if (newNode == NULL) return (NULL);
-
-    newNode->n = value;
-    newNode->parent = parent;
-    newNode->left = NULL;
-    newNode->right = NULL;
-
-    if (parent->left != NULL) {
-        currentNode = parent->left;
-        parent->left = newNode;
-        newNode->left = currentNode;
-        currentNode->parent = newNode;
-    } else {
-        parent->left = newNode;
-    }
-
-    return (newNode);
+	if (parent == NULL)
+		return (NULL);
+	newNode = malloc(sizeof(binary_tree_t));
+	if (newNode == NULL)
+		return (NULL);
+	newNode->n = value;
+	newNode->parent = parent;
+	newNode->left = NULL;
+	newNode->right = NULL;
+	if (parent->left != NULL)
+	{
+		currentNode = parent->left;
+		parent->left = newNode;
+		newNode->left = currentNode;
+		currentNode->parent = newNode;
+	} else
+	{
+		parent->left = newNode;
+	}
+	return (newNode);
 }
