06b91dfdbf85010ac2687831cc43029e99e724fb
Kamanda <erickckamanda@gmail.com>
2024-06-05 05:56:32 -0700

    Add a node to the right

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,33 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - Inserts a node as the left-child of another
+ * @parent: A pointer to the node to insert the right-child in
+ * @value: The value to store in the new node
+ *
+ * Return: A pointer to the created node, or NULL on failure
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newNode;
+	binary_tree_t *currentNode;
+
+	if (parent == NULL)
+		return (NULL);
+	newNode = malloc(sizeof(binary_tree_t));
+	if (newNode == NULL)
+		return (NULL);
+	newNode->n = value;
+	newNode->parent = parent;
+	newNode->left = NULL;
+	newNode->right = NULL;
+	if (parent->right != NULL)
+	{
+		currentNode = parent->right;
+		parent->right = newNode;
+		newNode->right = currentNode;
+		currentNode->parent = newNode;
+	}else
+		parent->right = newNode;
+	return (newNode);
+}
