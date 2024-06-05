f8f386b9d2aaef64effdb7724743a369fa0ddfbb
Kamanda <erickckamanda@gmail.com>
2024-06-05 05:09:11 -0700

    Fix betty documentation errors

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -8,15 +8,16 @@
  *
  * Return: Pointer to the new node or NULL if creation fails
 */
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value) {
-    binary_tree_t *node;
-    node = malloc(sizeof(binary_tree_t));
-    if (node == NULL) return (NULL);
-    
-    node->parent = parent;
-    node->n = value;
-    node->left = NULL;
-    node->right = NULL;
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node;
 
-    return (node);
+	node = malloc(sizeof(binary_tree_t));
+	if (node == NULL)
+		return (NULL);
+	node->parent = parent;
+	node->n = value;
+	node->left = NULL;
+	node->right = NULL;
+	return (node);
 }
